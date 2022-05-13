e652ff2aa96473a034a9d50cd533cf72ba634731
nicanorjm <julietnicanor1996@gmail.com>
2022-05-14 00:55:25 +0800

    Update app.js

--- app.js
+++ app.js
@@ -49,7 +49,8 @@ app.use(express.static("uploads"));
 
 //image upload for brgyid
 var storage = multer.diskStorage({destination: function(req, file, cb){
-    cb(null, './uploads');
+    // cb(null, './uploads');
+    cb(null, path.resolve(__dirname, 'uploads'));
   }, 
   filename: function(req, file, cb){
     cb(null, file.fieldname +"_"+ Date.now() +"_"+ file.originalname);
