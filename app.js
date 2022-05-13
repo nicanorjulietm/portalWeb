12027117fa92d0a2fe62191b5395953ca4c1832f
nicanorjm <julietnicanor1996@gmail.com>
2022-05-13 18:55:32 +0800

    Edit Port

--- app.js
+++ app.js
@@ -2670,6 +2670,9 @@ app.get("/adminviewreqidapproved/:id", (req,res, next ) =>{
 
 //---------------------------------------
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Server started on port 3000");
+// app.listen(process.env.PORT || 3000, function () {
+//   console.log("Server started on port 3000");
+// });
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
